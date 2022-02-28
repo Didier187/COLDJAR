99f62ac4cc1fd390596d99a9f9d38e0238df838a
Didier <Kdondidier@gmail.com>
2022-02-28 19:53:06 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# visualising where a user clicked and their scroll path
+
+Quick start:
+
+```javascript
+// clone this repo to your machine
+```
+
+open with live server
+
+---
+
+* Click anywhere on the page and the click see where you clicked button
+first click with be represented by a green dot and last one by a red dot
+
+* Click on see where scrolled! button
+start of mouse movement is represented by a green dot and stop / last mouse position is red
+
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,31 @@
+// javascript
+ let mouse_positions =[]
+ let scroll_positions = []
+ const body = document.getElementById("body")
+ window.addEventListener("online",function(){
+     console.log("loaded")
+ })
+window.addEventListener("click",function(e){
+    mouse_positions.push({x:e.clientX, y:e.clientY})
+})
+window.addEventListener("mousemove",function(e){
+    scroll_positions.push({x:e.clientX, y:e.clientY})
+})
+
+document.getElementById("vis-clicks").addEventListener("click",()=>{
+    if(mouse_positions.length){
+       let htmlElements = mouse_positions.map((position) => `
+       <span style="left:${position.x}px; top:${position.y}px" class="dot"></span>
+       `)
+      body.innerHTML = htmlElements.join("")
+    }
+})
+document.getElementById("vis-nav-path").addEventListener("click",()=>{
+    if(scroll_positions.length){
+       
+       let htmlElements = scroll_positions.map((position) => `
+       <span style="left:${position.x}px; top:${position.y}px" class="dot"></span>
+       `)
+      body.innerHTML = htmlElements.join("") 
+    }
+})
