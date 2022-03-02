f472e5c5f0b2e9230a970aa77f616ae02a07fe56
Didier <Kdondidier@gmail.com>
2022-03-02 13:04:26 +1100

    Better without conflicting changes

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 // javascript
-<<<<<<< HEAD
 let mouseClickLocations = [];
 let mousePositions = [];
 const body = document.getElementById("body");
-=======
- let mouse_positions =[]
- let scroll_positions = []
- const body = document.getElementById("body")
-
-window.addEventListener("click",function(e){
-    mouse_positions.push({x:e.clientX, y:e.clientY})
-})
-window.addEventListener("mousemove",function(e){
-    scroll_positions.push({x:e.clientX, y:e.clientY})
-})
->>>>>>> 3c12550067d669587689d3be5ceb939618df7a70
 
 window.addEventListener("click", function (e) {
     mouseClickLocations.push({ x: e.clientX, y: e.clientY });
@@ -40,14 +27,7 @@ const plot = (data) => {
   let htmlElements = data.map(
     (position) => `
        <span style="left:${position.x}px; top:${position.y}px" class="dot"></span>
-<<<<<<< HEAD
        `
   );
   body.innerHTML = htmlElements.join("");
 };
-=======
-       `)
-      body.innerHTML = htmlElements.join("") 
-    }
-})
->>>>>>> 3c12550067d669587689d3be5ceb939618df7a70
