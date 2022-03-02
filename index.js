8de272fa4464d9b32ac6e368a1563c4238d5dd87
Didier <Kdondidier@gmail.com>
2022-03-02 12:58:03 +1100

    DRY

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 // javascript
- let mouse_positions =[]
- let scroll_positions = []
- const body = document.getElementById("body")
- window.addEventListener("online",function(){
-     console.log("loaded")
- })
-window.addEventListener("click",function(e){
-    mouse_positions.push({x:e.clientX, y:e.clientY})
-})
-window.addEventListener("mousemove",function(e){
-    scroll_positions.push({x:e.clientX, y:e.clientY})
-})
+let mouseClickLocations = [];
+let mousePositions = [];
+const body = document.getElementById("body");
 
-document.getElementById("vis-clicks").addEventListener("click",()=>{
-    if(mouse_positions.length){
-       let htmlElements = mouse_positions.map((position) => `
+window.addEventListener("click", function (e) {
+    mouseClickLocations.push({ x: e.clientX, y: e.clientY });
+});
+window.addEventListener("mousemove", function (e) {
+    mousePositions.push({ x: e.clientX, y: e.clientY });
+});
+
+document.getElementById("vis-clicks").addEventListener("click", () => {
+  if (mouseClickLocations.length) {
+    plot(mouseClickLocations);
+    body.innerHTML = htmlElements.join("");
+  }
+});
+
+document.getElementById("vis-nav-path").addEventListener("click", () => {
+  if (mousePositions.length) {
+    plot(mousePositions);
+  }
+});
+
+const plot = (data) => {
+  let htmlElements = data.map(
+    (position) => `
        <span style="left:${position.x}px; top:${position.y}px" class="dot"></span>
-       `)
-      body.innerHTML = htmlElements.join("")
-    }
-})
-document.getElementById("vis-nav-path").addEventListener("click",()=>{
-    if(scroll_positions.length){
-       
-       let htmlElements = scroll_positions.map((position) => `
-       <span style="left:${position.x}px; top:${position.y}px" class="dot"></span>
-       `)
-      body.innerHTML = htmlElements.join("") 
-    }
-})
+       `
+  );
+  body.innerHTML = htmlElements.join("");
+};
